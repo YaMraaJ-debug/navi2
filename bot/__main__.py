fc1902f80dd3913ebde574154883915a0cda8cdc
ronin08 <75478274+ronin08@users.noreply.github.com>
2021-05-30 14:47:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,7 +56,7 @@ def start(update, context):
 This bot can mirror all your links to Google drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-    update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
+    update.effective_message.reply_photo(IMAGE_URL)
 
 
 @run_async
