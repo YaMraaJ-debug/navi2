11412bfd81b7f55174b67f2a604b097f0c352b06
ronin08 <75478274+ronin08@users.noreply.github.com>
2021-05-31 12:38:05 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -98,7 +98,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded Mirror Bot',
+            'description': 'Uploaded using Mirror Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
