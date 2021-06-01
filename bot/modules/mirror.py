1a70896a438e24ef4e104c7a1dd5df2b9028c298
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2021-06-01 19:19:42 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -148,14 +148,10 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str, size):
         with download_dict_lock:
-            msg = f'<b>📂 Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>Size: </b><code>{size}</code>\n' \
-                  f' \n' \
-                  f'📍 Join Our Team Drive To Gain Access To The G-Drive Link.\n' \
-                  f'📍 Do Not Share G-Drive / Index Links Outside the Group.\n' \
-                  f'💫 Mirror∆Cloud Join @mirrorclouds\n' \
+            msg = f'<b>📂 Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>Size: </b><code>{size}</code>'
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
-                surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={link}&format=text').text
+                surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, link)).text
                 buttons.buildbutton("☁️Drive Link☁️", surl)
             else:
                 buttons.buildbutton("☁️Drive Link☁️", link)
